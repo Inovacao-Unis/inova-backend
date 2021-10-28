979900fb36cf361a110e8cb5b5fe0447b2236156
patrickpereirak <patrickpereirak@gmail.com>
2021-10-28 14:39:50 -0300

    replace update

--- src/utils/googleKey.js
+++ src/utils/googleKey.js
@@ -2,7 +2,7 @@ module.exports = {
   "type": process.env.G_TYPE,
   "project_id": process.env.G_PROJECT_ID,
   "private_key_id": process.env.G_KEY_ID,
-  "private_key": process.env.G_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  "private_key": process.env.G_PRIVATE_KEY.replace(new RegExp("\\\\n", "\g"), "\n"),
   "client_email": process.env.G_CLIENT_EMAIL,
   "client_id": process.env.G_CLIENT_ID,
   "auth_uri": process.env.G_AUTH_URI,
