418b6587b7509dd59e5339a486ba2e2109084bca
patrickpereirak <patrickpereirak@gmail.com>
2021-12-08 14:41:27 -0300

    fix merge

--- src/controllers/GameController.js
+++ src/controllers/GameController.js
@@ -1,16 +1,12 @@
 const mongoose = require('mongoose');
 const Team = require("../models/Team");
-<<<<<<< HEAD
 const Challenge = require("../models/Challenge");
 const Category = require("../models/Category");
-=======
->>>>>>> main
 const Point = require("../models/Point");
 const Leader = require("../models/Leader");
 const Trail = require("../models/Trail");
 const Response = require("../models/Response");
 const admin = require("firebase-admin");
-const { response } = require('express');
 
 module.exports = {
   async team(req, res) {
@@ -60,7 +56,6 @@ module.exports = {
 
     result.progress = (responses.length * 100) / 4;
 
-<<<<<<< HEAD
     const challenge = await Challenge.findById(team.challengeId);
 
     const category = await Category.findById(challenge.categoryId);
@@ -68,8 +63,6 @@ module.exports = {
     result.challenge = challenge;
     result.category = category;
 
-=======
->>>>>>> main
     return res.json(result);
   },
 
